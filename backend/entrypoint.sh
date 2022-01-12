f45653ad21cf3ce42ab857847800315b8886d918
Tomas Drozdik <tdrozdik@purestorage.com>
2022-01-12 15:15:22 +0100

    Add docker compose structure

---
+++ backend/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if [ "$DATABASE" = "postgres" ]
+then
+  echo "Waiting for postgres..."
+
+  while ! nc -z $SQL_HOST $SQL_PORT; do
+    sleep 0.1
+  done
+
+  echo "PostgreSQL started"
+fi
+
+if [ "$ENVIRONMENT" = "test" ]
+then
+  echo "Creating the database tables..."
+  python manage.py makemigrations
+  python manage.py migrate
+  echo "Tables created."
+fi
+
+exec "$@"
